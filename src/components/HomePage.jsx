6a7294daeda693f6e43d4c54d1e1c91ecf2311a3
Reid Hanson <reidhans@buffalo.edu>
2019-11-25 21:51:29 -0500

    new links

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const HomePage = () => {
   return (
     <>
@@ -9,20 +9,23 @@ const HomePage = () => {
           <p>
             What need I say? Probably the most iconic band of all time. Formed
             in the 60's, in Liverpool, The Beatles quickly rose to global fame.
+            Click <Link to="/beatles">Here</Link> to find out more
           </p>
         </div>
         <div className="coldPlay">
           <h1>Coldplay</h1>
           <p>
             Another iconic modern British band, known for classics such as "Fix
-            You" and "Yellow"
+            You" and "Yellow". Click <Link to="/coldplay">Here</Link> to find
+            out more
           </p>
         </div>
         <div className="greenDay">
           <h1>Greenday</h1>
           <p>
             A band that rose to fame in the early 2000s with their controversial
-            album "American Idiot"
+            album "American Idiot". Click <Link to="/greenday">Here</Link> to
+            find out more.{' '}
           </p>
         </div>
       </main>
